0d480f4dd2c02ade60e82f95e66378ff1352ad39
Ahmed Anwar El Kafrawy <Ahmed.Kafrawy@Egypt.TE-Data.core>
2025-12-25 14:21:32 +0200

    add alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,13 +2,13 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 -- AUTHORS TABLE
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- BOOKS TABLE
-CREATE TABLE books (
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE books (
 );
 
 -- CUSTOMERS TABLE
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE customers (
 );
 
 -- ORDERS TABLE
-CREATE TABLE orders (
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE,
@@ -42,7 +42,7 @@ CREATE TABLE orders (
 );
 
 -- ORDER DETAILS TABLE
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
