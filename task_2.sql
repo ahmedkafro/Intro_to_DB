fde44394b22e4b501e687cdce485193915878e70
Ahmed Anwar El Kafrawy <Ahmed.Kafrawy@Egypt.TE-Data.core>
2025-12-25 16:05:10 +0200

    add task_2.sql

---
+++ task_2.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE IF NOT EXISTS task_2;
+USE task_2;
+
+-- AUTHORS TABLE
+CREATE TABLE authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- BOOKS TABLE
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    CONSTRAINT fk_books_author
+        FOREIGN KEY (author_id)
+        REFERENCES authors(author_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
+
+-- CUSTOMERS TABLE
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+);
+
+-- ORDERS TABLE
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE DEFAULT (CURRENT_DATE),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- ORDER DETAILS TABLE
+CREATE TABLE order_details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
